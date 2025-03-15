fb3dd84f85b2df7319e68cad875d0b9699ae1ab3
SmallTing1124 <chentt1124@gmail.com>
2025-03-15 22:30:25 +0800

    資料更新

--- server.js
+++ server.js
@@ -6,11 +6,24 @@ const auth = require("json-server-auth");
 const db = require("./db.json");
 const router = jsonServer.router(db);
 const middlewares = jsonServer.defaults();
+
+// 設置 CORS 允許所有來源
 server.use(cors())
+
+// 設置預設中介軟體
 server.use(middlewares)
+
+// 設置路由資料庫
 server.db = router.db;
+
+// 啟用 json-server-auth 中介軟體
 server.use(auth);
+
+
+// 配置 json-server 路由
 server.use(router)
+
+// 啟動伺服器
 server.listen(3000, () => {
     console.log('JSON Server is running')
 })
